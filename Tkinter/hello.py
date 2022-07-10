731afb20728a3e43761a42c091ac2de5bf625a20
Corimo <MattCarlile590@gmail.com>
2022-07-10 22:24:47 +1000

    Tkinter

---
+++ Tkinter/hello.py
@@ -0,0 +1,7 @@
+import tkinter
+
+root = tkinter.Tk()
+message = tkinter.Label(root, text='Hello World!')
+message.pack()
+
+root.mainloop()
---
+++ Tkinter/window_manipulation.py
@@ -0,0 +1,24 @@
+import tkinter
+from tkinter import ttk
+
+poop = tkinter.Tk()
+poop.title("Tkinter Window Title")
+
+window_height = 300
+window_width = 600
+width_screen = poop.winfo_screenwidth()
+height_screen = poop.winfo_screenheight()
+centre_width = int(width_screen/2 - window_height/2)
+centre_height = int(height_screen/2 - window_height/2)
+
+poop.resizable(False, False)
+poop.minsize(300, 150)
+poop.maxsize(1200, 600)
+poop.attributes('-alpha', 0.9)
+poop.attributes("-topmost", True)
+poop.iconbitmap('Tkinter\icon\pythontutorial.ico')
+poop.configure(background="#333333")
+
+poop.geometry(f'{window_width}x{window_height}+{centre_width}+{centre_height}')
+
+poop.mainloop()
